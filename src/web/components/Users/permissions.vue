04cd80b95c1670378661ad6a918f390cb1ff8d87
Saxon Landers <saxon@punchbuggy.com.au>
2018-09-06 12:52:22 +1000

    Initial frontend handling for permissions

--- src/web/components/Users/permissions.vue
+++ src/web/components/Users/permissions.vue
@@ -1,8 +1,38 @@
 <template>
 <div class="main">
   <app-nav></app-nav>
-  <div v-for="user in list" :key="user.id">
+  <div v-for="user in list" :key="user.id" class="container">
     {{ user }}
+    <h4>{{ user.email || user.id }}</h4>
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th v-for="role in roles" :key="role" scope="col">
+            {{ role }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in user.projects" :key="project.id">
+          <th scope="row">{{ project.name }}</th>
+          <td v-for="role in roles" :key="role">
+            <input
+              type="checkbox"
+              :id="user.id + '|' + project.id + '|' + role"
+              :checked="project.permissions.includes(role)"
+              @change="setPermission({
+                user: user.id,
+                project: project.id,
+                role,
+                value: $event.target.checked
+              })"
+            >
+            <label :for="user.id + '|' + project.id + '|' + role"></label>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </div>
 </template>
@@ -15,10 +45,21 @@ export default {
   components: {
     appNav,
   },
+  data: () => ({
+    roles: [
+      'read',
+      'edit',
+      'own',
+      'admin',
+    ]
+  }),
   created() {
     this.loadUsers()
   },
-  methods: Vuex.mapActions('user', ['loadUsers']),
+  methods: {
+    ...Vuex.mapActions('user', ['loadUsers']),
+    ...Vuex.mapMutations('user', ['setPermission']),
+  },
   computed: Vuex.mapState('user', ['list'])
 }
 </script>
--- src/web/store/modules/user/mutations.js
+++ src/web/store/modules/user/mutations.js
@@ -6,6 +6,23 @@ export default {
     state.list = result
   },
 
+  setPermission(state, payload) {
+    const user = state.list.find(user => user.id === payload.user)
+    if (!user) { return }
+
+    const project = user.projects.find(project => project.id === payload.project)
+    if (!project) { return }
+
+    if (payload.value) {
+      project.permissions.push(payload.role)
+    } else {
+      const index = project.permissions.indexOf(payload.role)
+      if (index > -1) {
+        project.permissions.splice(index, 1)
+      }
+    }
+  },
+
   // loadUser(state, user) {
   //   state.user = user;
   //   router.push(`/user/${user.id}`);
